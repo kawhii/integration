4044834b1d590bfa1eb9773ca17de7de4aff85c3
laomazi2006 <756884434@qq.com>
2016-12-22 01:06:29 +0800

    1.重写校验过滤器 2.加上基础的获取用户数据逻辑

---
+++ breakfast-web/src/main/java/com/carl/breakfast/web/filter/UserInfoAuthenticationFilter.java
@@ -0,0 +1,28 @@
+package com.carl.breakfast.web.filter;
+
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * 把用户信息设到session中
+ *
+ * @author Carl
+ * @date 2016/12/22
+ * 版权所有.(c)2016 - 2020. 卡尔工作室
+ */
+public class UserInfoAuthenticationFilter extends FormAuthenticationFilter {
+    //当前用户的用户信息
+    public static final String USER_INFO_ATTR_KEY = "UserInfo";
+
+    @Override
+    protected boolean onLoginSuccess(AuthenticationToken token, Subject subject, ServletRequest request, ServletResponse response) throws Exception {
+        String username = (String)subject.getPrincipal();
+        //todo，根据用户名查询用户信息，保存在session中
+//        subject.getSession().setAttribute(USER_INFO_ATTR_KEY, );
+        return super.onLoginSuccess(token, subject, request, response);
+    }
+}
