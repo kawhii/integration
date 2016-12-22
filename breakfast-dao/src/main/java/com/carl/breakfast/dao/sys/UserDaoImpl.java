cb0920a5a5ddefe38784f6fd40d87da34f841cf5
laomazi2006 <756884434@qq.com>
2016-12-22 23:44:49 +0800

    1.用户dao基本代码

---
+++ breakfast-dao/src/main/java/com/carl/breakfast/dao/sys/UserDaoImpl.java
@@ -0,0 +1,12 @@
+package com.carl.breakfast.dao.sys;
+
+import com.carl.breakfast.dao.sys.pojo.UserInfo;
+import com.carl.framework.core.dao.BaseDaoImpl;
+
+/**
+ * @author Carl
+ * @date 2016/12/22
+ * 版权所有.(c)2016 - 2020. 卡尔工作室
+ */
+public class UserDaoImpl extends BaseDaoImpl<UserInfo> {
+}
