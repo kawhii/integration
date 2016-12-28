85334654abece0c3ead8624b48297f7a01f22fd4
Carl <75688443@qq.com>
2016-12-28 19:12:13 +0800

    添加jms

---
+++ breakfast-dao/src/main/java/com/carl/breakfast/dao/config/JmsConfig.java
@@ -0,0 +1,18 @@
+package com.carl.breakfast.dao.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+
+/**
+ * jms
+ *
+ * @author Carl
+ * @date 2016/12/28
+ * @since JDK1.7
+ * <p>
+ * 版权所有.(c)2008-2016.卡尔工作室
+ */
+@Configuration
+@EnableJms
+public class JmsConfig {
+}
---
+++ breakfast-dao/src/test/java/com/carl/breakfast/dao/config/ActiveMqJmsConfigTest.java
@@ -0,0 +1,131 @@
+package com.carl.breakfast.dao.config;
+
+import org.junit.Test;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Create by Administrator on 2016/12/28.
+ */
+public class ActiveMqJmsConfigTest {
+
+    @Test
+    public void producer() throws InterruptedException {
+        Thread t = thread(new HelloWorldProducer(), false);
+        t.join();
+    }
+
+    @Test
+    public void consumer() throws InterruptedException {
+        Thread t = thread(new HelloWorldConsumer(), false);
+        t.join();
+    }
+
+    public static Thread thread(Runnable runnable, boolean daemon) {
+        Thread brokerThread = new Thread(runnable);
+        brokerThread.setDaemon(daemon);
+        brokerThread.start();
+        return brokerThread;
+    }
+
+    public static class HelloWorldProducer implements Runnable {
+        public void run() {
+            try {
+                // Create a ConnectionFactory
+
+                ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(/*"vm://localhost"*/);
+
+                // Create a Connection
+                Connection connection = connectionFactory.createConnection();
+                connection.start();
+
+                // Create a Session
+                Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+                // Create the destination (Topic or Queue)
+                Destination destination = session.createQueue("TEST.FOO");
+
+                // Create a MessageProducer from the Session to the Topic or Queue
+                MessageProducer producer = session.createProducer(destination);
+                producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+                // Create a messages
+                String text = "Hello world! From: " + Thread.currentThread().getName() + " : " + this.hashCode();
+                TextMessage message = session.createTextMessage(text);
+
+                // Tell the producer to send the message
+                System.out.println("Sent message: " + message.hashCode() + " : " + Thread.currentThread().getName());
+                producer.send(message);
+
+                // Clean up
+                session.close();
+                connection.close();
+            } catch (Exception e) {
+                System.out.println("Caught: " + e);
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public static class HelloWorldConsumer implements Runnable, ExceptionListener {
+        public void run() {
+            try {
+
+                // Create a ConnectionFactory
+                ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+
+                // Create a Connection
+                Connection connection = connectionFactory.createConnection();
+                connection.start();
+
+                connection.setExceptionListener(this);
+
+                // Create a Session
+                Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+                // Create the destination (Topic or Queue)
+                Destination destination = session.createQueue("TEST.FOO");
+
+                // Create a MessageConsumer from the Session to the Topic or Queue
+                MessageConsumer consumer = session.createConsumer(destination);
+
+                consumer.setMessageListener((message) -> {
+                    if (message instanceof TextMessage) {
+                        TextMessage textMessage = (TextMessage) message;
+                        String text = null;
+                        try {
+                            text = textMessage.getText();
+                        } catch (JMSException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println("Received: " + text);
+                    } else {
+                        System.out.println("Received: " + message);
+                    }
+                });
+
+                // Wait for a message
+//                Message message = consumer.receive(1000);
+
+                Thread.sleep(20000);
+
+                consumer.close();
+                session.close();
+                connection.close();
+            } catch (Exception e) {
+                System.out.println("Caught: " + e);
+                e.printStackTrace();
+            }
+        }
+
+        public synchronized void onException(JMSException ex) {
+            System.out.println("JMS Exception occured.  Shutting down client.");
+        }
+    }
+}
+
