47f1aa96bec0bec49c26f86b1cded65746382fbd
laomazi2006 <756884434@qq.com>
2017-01-01 17:37:07 +0800

    新增地址的插入sql 调整地址表为自增id

--- breakfast-dao/src/main/java/com/carl/breakfast/dao/user/SendAddressDaoImpl.java
+++ breakfast-dao/src/main/java/com/carl/breakfast/dao/user/SendAddressDaoImpl.java
@@ -2,6 +2,7 @@ package com.carl.breakfast.dao.user;
 
 import com.carl.breakfast.dao.pojo.user.SendAddress;
 import com.carl.framework.core.dao.BaseDaoImpl;
+import org.springframework.stereotype.Repository;
 
 /**
  * 收集信息默认dao
@@ -9,5 +10,6 @@ import com.carl.framework.core.dao.BaseDaoImpl;
  * @date 2017/1/1
  * 版权所有.(c)2017 - 2020. 卡尔工作室
  */
+@Repository("addressDao")
 public class SendAddressDaoImpl extends BaseDaoImpl<SendAddress> implements ISendAddressDao {
 }
