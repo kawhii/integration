dd913bb8decd262fab72260abc360e91366f8fed
Carl <75688443@qq.com>
2017-01-24 10:48:26 +0800

    添加控制器修改密码 整个流程尚未测试

--- breakfast-web/src/main/java/com/carl/breakfast/web/ctrl/admin/AdminCtrl.java
+++ breakfast-web/src/main/java/com/carl/breakfast/web/ctrl/admin/AdminCtrl.java
@@ -1,11 +1,14 @@
 package com.carl.breakfast.web.ctrl.admin;
 
+import com.carl.breakfast.web.service.IUserService;
+import com.carl.breakfast.web.service.UsesModifyFlag;
 import com.carl.framework.ui.ctrl.BaseCtrl;
 import com.carl.framework.util.StringUtil;
+import org.apache.shiro.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
@@ -21,6 +24,9 @@ import java.util.Map;
 @Controller
 @RequestMapping("/admin")
 public class AdminCtrl extends BaseCtrl {
+
+    @Autowired
+    private IUserService userService;
 
     @Value("${url.admin.default.url}")
     private String defaultUrl;
@@ -43,5 +49,41 @@ public class AdminCtrl extends BaseCtrl {
         //根据这个url来定位跳转父链接
         params.put("parentUrl", indexUrl.substring(indexUrl.lastIndexOf("/") + 1));
         return new ModelAndView(freemarker("index"), params);
+    }
+
+    @RequestMapping(value = "/modifyPwd.do", method = RequestMethod.POST)
+    @ResponseBody
+    public Object modifyPwd(@RequestBody ModifyPwdBean modifyPwdBean) {
+        String username = (String) SecurityUtils.getSubject().getPrincipal();
+        UsesModifyFlag flag = userService.modifyPassword(username, modifyPwdBean.oldPwd, modifyPwdBean.newPwd);
+        switch (flag) {
+            case SUCCESS:
+                return success();
+            default:
+                return fail("修改失败");
+        }
+    }
+
+    public static class ModifyPwdBean {
+        private String oldPwd;
+        private String newPwd;
+
+        public String getOldPwd() {
+            return oldPwd;
+        }
+
+        public ModifyPwdBean setOldPwd(String oldPwd) {
+            this.oldPwd = oldPwd;
+            return this;
+        }
+
+        public String getNewPwd() {
+            return newPwd;
+        }
+
+        public ModifyPwdBean setNewPwd(String newPwd) {
+            this.newPwd = newPwd;
+            return this;
+        }
     }
 }
